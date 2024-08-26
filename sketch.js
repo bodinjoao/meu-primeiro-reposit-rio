bb0c65ac5db92ba5ae009beacfe09f6d28d9b707
bodinjoao <franca.joao14@escola.pr.gov.br>
2024-08-26 11:41:31 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,23 @@
+
+ function setup() {
+  createCanvas(400, 400);
+  
+function segundaLei(massa,aceleracao){
+
+   
+  let forca=massa*aceleracao;
+  console.log(forca);  
+  return forca;
+
+ }   
+ segundaLei(10,20);
+
+ } 
+ 
+
+
+
+ function draw() {
+
+ 
+ }
